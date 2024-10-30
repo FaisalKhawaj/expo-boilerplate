683922b4d85a92102d129b9a98dd78e3917bf7a1
FaisalKhawaj <khawajfaisal981@gmail.com>
2024-10-30 16:58:29 +0500

    added keyboard avoiding view

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,5 +1,15 @@
 import { Redirect, router } from "expo-router";
-import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Dimensions,
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TouchableOpacity,
+  Platform,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { LoginFormType } from "./login.interface";
@@ -11,6 +21,8 @@ import { CustomTextInput } from "@/components/CustomTextInput";
 import { Colors } from "@/constants/Colors";
 import { useAuth } from "@/src/context/AuthContext";
 import { SecureStorageHelper } from "@/src/helpers/SecureStorageHelper";
+
+const { width, height } = Dimensions.get("window");
 
 export const Login = () => {
   const { loginSchema } = useValidations();
@@ -44,65 +56,77 @@ export const Login = () => {
     return <Redirect href={"/(tabs)"} />;
   }
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.light.background }}>
-      <View style={{ flexGrow: 1, padding: 20 }}>
-        <Text style={globalStyles.screenTitle}>Sign in</Text>
-        <View style={{ marginTop: 20 }} />
-
-        <View style={{ marginTop: 50 }} />
-        <Text style={globalStyles.fieldTitle}>Email</Text>
-        <CustomTextInput
-          control={control}
-          name={"email"}
-          isPortrait={true}
-          keyboardType="email-address"
-          returnKeyType="done"
-          isRequired={true}
-          secureTextEntry={false}
-          placeholderColor={""}
-          maxLength={70}
-          showToggleEye={false}
-          multiLine={false}
-          // style={{ width: containerWidth }}
-        />
-
-        <View style={{ marginTop: 30 }} />
-        <Text style={globalStyles.fieldTitle}>Password</Text>
-        <CustomTextInput
-          control={control}
-          name={"password"}
-          keyboardType="default"
-          returnKeyType="done"
-          isRequired={true}
-          placeholderColor={""}
-          maxLength={70}
-          multiLine={false}
-          secureTextEntry={hidePassword}
-          showToggleEye={true}
-          isPortrait={true}
-          handleToggleEye={togglePassword}
-          // style={{ width: containerWidth }}
-        />
-        <View style={{ marginTop: 30 }} />
-        <TouchableOpacity
-          activeOpacity={0.8}
-          // onPress={handleLogin}
-          onPress={handleSubmit(handleLogin)}
-          style={[globalStyles.buttonStyle, {}]}
-        >
-          <Text style={[globalStyles.buttonText]}>Login</Text>
-        </TouchableOpacity>
-        <View style={{ marginTop: 20 }} />
-        <Text style={[globalStyles.dontHaveAcc, { textAlign: "center" }]}>
-          Don't have an account?{" "}
-          <Text
-            onPress={() => router.push("/signup")}
-            style={{ fontFamily: fonts.primary.bold }}
+    <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+      <SafeAreaView
+        style={{ flex: 1, backgroundColor: Colors.light.background }}
+      >
+        <View style={{ flexGrow: 1, padding: 20 }}>
+          <KeyboardAvoidingView
+            behavior="position"
+            keyboardVerticalOffset={
+              height < 600 ? -50 : Platform.OS == "ios" ? -60 : -120
+            }
+            style={{ flexGrow: 1, padding: 20 }}
           >
-            Sign up
-          </Text>
-        </Text>
-      </View>
-    </SafeAreaView>
+            <Text style={globalStyles.screenTitle}>Sign in</Text>
+            <View style={{ marginTop: 20 }} />
+
+            <View style={{ marginTop: 50 }} />
+            <Text style={globalStyles.fieldTitle}>Email</Text>
+            <CustomTextInput
+              control={control}
+              name={"email"}
+              isPortrait={true}
+              keyboardType="email-address"
+              returnKeyType="done"
+              isRequired={true}
+              secureTextEntry={false}
+              placeholderColor={""}
+              maxLength={70}
+              showToggleEye={false}
+              multiLine={false}
+              // style={{ width: containerWidth }}
+            />
+
+            <View style={{ marginTop: 30 }} />
+            <Text style={globalStyles.fieldTitle}>Password</Text>
+            <CustomTextInput
+              control={control}
+              name={"password"}
+              keyboardType="default"
+              returnKeyType="done"
+              isRequired={true}
+              placeholderColor={""}
+              maxLength={70}
+              multiLine={false}
+              secureTextEntry={hidePassword}
+              showToggleEye={true}
+              isPortrait={true}
+              handleToggleEye={togglePassword}
+              // style={{ width: containerWidth }}
+            />
+            <View style={{ marginTop: 30 }} />
+            <TouchableOpacity
+              activeOpacity={0.8}
+              // onPress={handleLogin}
+              onPress={handleSubmit(handleLogin)}
+              style={[globalStyles.buttonStyle, {}]}
+            >
+              <Text style={[globalStyles.buttonText]}>Login</Text>
+            </TouchableOpacity>
+            <View style={{ marginTop: 20 }} />
+            <Text style={[globalStyles.dontHaveAcc, { textAlign: "center" }]}>
+              Don't have an account?{" "}
+              <Text
+                onPress={() => router.push("/signup")}
+                style={{ fontFamily: fonts.primary.bold }}
+              >
+                Sign up
+              </Text>
+            </Text>
+          </KeyboardAvoidingView>
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
