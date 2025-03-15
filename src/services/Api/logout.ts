b21aec7f95c7df32cae9b72bb3efa34b13a30c5a
Faisal Khawaj <56731703+FaisalKhawaj@users.noreply.github.com>
2025-03-16 00:12:53 +0500

    Delete src/services/Api directory

--- src/services/Api/logout.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "../api/api";
-import { getDeviceId } from "../notifications/notifications";
-
-export const logout = async (): Promise<void> => {
-  const deviceId = getDeviceId();
-  await api.post("/auth/logout", { deviceId });
-};
--- src/services/Api/me.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "../api/api";
-import { User } from "../auth/types";
-import { handleAuthResponse } from "../utils";
-
-export const getMe = async (): Promise<User> => {
-  const response = await api.get<User>("/auth/me");
-  return handleAuthResponse(response);
-};
